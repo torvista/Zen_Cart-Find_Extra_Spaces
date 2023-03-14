0990db816a635f6da381673cac6baabb9d03771e
Steve <4391026+torvista@users.noreply.github.com>
2023-03-14 11:48:46 +0100

    misc non-functional changes, HTML5, IDE recommendations

--- shop root/find_extra_spaces.php
+++ shop root/find_extra_spaces.php
@@ -1,4 +1,6 @@
 <?php
+
+declare(strict_types=1);
 /**
  * @package admin
  * @copyright Copyright 2005-2009, Andrew Berezin eCommerce-Service.com
@@ -34,21 +36,70 @@ $basedir = getcwd();
 $basedir_strlen = strlen($basedir);
 $files = rdir($basedir, '@\.php$@', 1);
 $time['rdir'] = (microtime_float()-$time_start);
-//echo timefmt($time['rdir']) . "<br />\n";
+//echo timefmt($time['rdir']) . "<br>\n";
 
 $bad_files = 0;
 $canWrite = false;
 $time_start = microtime_float();
 
-//echo 'Directory: ' . $basedir . "<br />\n";
+//echo 'Directory: ' . $basedir . "<br>\n";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<title>Zen Cart - Find Extra Spaces</title>
+<style>
+    body {
+        font-size: 12px;
+        font-family: Verdana, Arial, Helvetica, sans-serif;
+    }
+    h1 {
+        font-size: 18px;
+    }
+    h2 {
+        font-size: 16px;
+    }
+    table {
+        color: #333;
+        background: white;
+        border: 1px solid grey;
+        font-size: 12pt;
+        border-collapse: collapse;
+    }
+    table thead th,
+    table tfoot th {
+        color: #777;
+        background: rgba(0,0,0,.1);
+    }
+    table caption {
+        padding:.5em;
+    }
+    table th,
+    table td {
+        padding: .5em;
+        border: 1px solid lightgrey;
+    }
+</style>
+</head>
+<body>
+<h1>Zen Cart - Find Extra Spaces</h1>
+<h2>Scan includes Admin files</h2>
+<p>Note that two core files deliberately have an extra space at the end of the file/should not be modified:</p>
+<ul>
+    <li>/includes/templates/responsive_classic/common/html_header.php</li>
+    <li>/includes/templates/template_default/common/html_header.php</li>
+</ul>
+<?php
 echo '<form action="' . basename($_SERVER['SCRIPT_NAME']) . '?action=edit" method="post">' . "\n";
-echo '<table border="1" cellpadding="2" cellspacing="0">' . "\n";
+echo '<table>' . "\n";
 echo '<tr>';
-echo '  <th>Edit</th>';
-echo '  <th>File</th>';
+echo '  <th>Modify?</th>';
+echo '  <th style="text-align:left">File</th>';
 echo '  <th><a href="https://en.wikipedia.org/wiki/Byte-order_mark" target="_blank">BOM</a></th>';
-echo '  <th>Extra space<br />on top</th>';
-echo '  <th>Extra space<br />on bottom</th>';
+echo '  <th>Extra space<br>on top</th>';
+echo '  <th>Extra space<br>on bottom</th>';
 echo '  <th>Message</th>';
 echo '</tr>' . "\n";
 foreach ($files as $file) {
@@ -102,46 +153,49 @@ foreach ($files as $file) {
       }
     }
     echo '<tr>' .
-         '  <td align="center">' . $boxExtra . '</td>' .
+         '  <td style="text-align:center">' . $boxExtra . '</td>' .
          '  <td>' . $file . '</td>' .
-         '  <td align="center">' . $flag_BOM . '</td>' .
-         '  <td align="center">' . $flag_START . '</td>' .
-         '  <td align="center">' . $flag_END . '</td>' .
-         '  <td align="center">' . $message . '</td>' .
+         '  <td style="text-align:center">' . $flag_BOM . '</td>' .
+         '  <td style="text-align:center">' . $flag_START . '</td>' .
+         '  <td style="text-align:center">' . $flag_END . '</td>' .
+         '  <td style="text-align:center">' . $message . '</td>' .
          '</tr>' . "\n";
     $bad_files++;
   }
 
 }
 echo '</table>' . "\n";
-echo 'Total files - ' . sizeof($files) . "<br />\n";
-echo 'Bad files - ' . $bad_files . "<br />\n";
+echo '<p>Total files - ' . sizeof($files) . "</p>\n";
+echo '<p>"Bad" files - ' . $bad_files . "<p>\n";
 $time['read'] = (microtime_float()-$time_start);
 if ($canWrite) {
-  echo 'Replace ending "?'.'>" by "// EOF" ?' . '<input name="change" value="yes" type="checkbox" checked>' . "<br />\n";
+  echo '<p>Replace ending "?'.'>" by "// EOF" ?' . '<input name="change" value="yes" type="checkbox">' . "</p>\n";
   if (false) {
 ?>
   <fieldset>
     <legend>FTP Access:</legend>
     <label for="ftp-Server" style="width: 20em;">Server:</label>
     <input name="ftp[Server]" id="ftp-Server" size="40" type="text">
-    <br />
+    <br>
     <label for="ftp-User" style="width: 20em;">User:</label>
     <input name="ftp[User]" id="ftp-User" size="16" type="text">
-    <br />
+    <br>
     <label for="ftp-Password" style="width: 20em;">Password:</label>
     <input name="ftp[Password]" id="ftp-Password" size="16" type="text">
-    <br />
+    <br>
     <label for="ftp-Directory" style="width: 20em;">Directory:</label>
     <input name="ftp[Directory]" id="ftp-Directory" size="80" type="text">
   </fieldset>
 <?php
   }
-  echo '<input class="button" value="Replace selected files" type="submit">';
+  echo '<input class="button" value="Modify selected files" type="submit">';
 }
 echo '</form>';
-//echo timefmt($time['read']) . "<br />\n";
-
+//echo timefmt($time['read']) . "<br>\n";
+?>
+</body>
+</html>
+<?php
 /**
  * @param $path
  * @param bool $patterns
